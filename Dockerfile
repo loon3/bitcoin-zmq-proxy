d1ec9162b00f1db762d67f4ce8afc7afcfaa3ca9
Joe Looney <hello@joelooney.org>
2024-07-08 10:01:22 -0400

    interpret rawblock

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 # Use an official Python runtime as a parent image
 FROM python:3.9-slim
 
-# Set the working directory
+# Set the working directory in the container
 WORKDIR /app
 
 # Copy the current directory contents into the container at /app
 COPY . /app
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir websockets pyzmq
+# Install any needed packages
+RUN pip install --no-cache-dir websockets pyzmq python-bitcoinlib
 
 # Make port 8765 available to the world outside this container
 EXPOSE 8765
--- proxy.py
+++ proxy.py
@@ -2,32 +2,47 @@ import asyncio
 import websockets
 import zmq
 import zmq.asyncio
+import json
+from bitcoin.core import CBlock
+from bitcoin.core.serialize import deserialize
+from bitcoin.core import lx
 
-BITCOIN_ZMQ_ADDRESS = "tcp://counterparty-core-bitcoind-1:9333"  # Change this to your Bitcoin ZMQ address
+BITCOIN_ZMQ_ADDRESS = "tcp://bitcoind:9333"  # Use the container name as the address
 WEBSOCKET_PORT = 8765
 
-async def zmq_listener(socket):
+async def zmq_listener():
+    context = zmq.asyncio.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect(BITCOIN_ZMQ_ADDRESS)
+    socket.setsockopt_string(zmq.SUBSCRIBE, 'rawblock')
+
     while True:
         msg = await socket.recv_multipart()
         print(f"Received ZMQ message: {msg}")
         yield msg
 
-async def zmq_listener_task(websocket):
-    context = zmq.asyncio.Context()
-    socket = context.socket(zmq.SUB)
-    socket.connect(BITCOIN_ZMQ_ADDRESS)
-    socket.setsockopt_string(zmq.SUBSCRIBE, '')
+def get_block_info(raw_block):
+    block = CBlock.deserialize(raw_block)
+    block_info = {
+        'height': block.nHeight,
+        'time': block.nTime
+    }
+    return block_info
 
-    async for msg in zmq_listener(socket):
-        print(f"Sending WebSocket message: {msg}")
-        await websocket.send(str(msg))
+async def zmq_listener_task(websocket):
+    async for msg in zmq_listener():
+        raw_block = msg[1]
+        block_info = get_block_info(raw_block)
+        block_info_json = json.dumps(block_info)
+        print(f"Sending WebSocket message: {block_info_json}")
+        await websocket.send(block_info_json)
 
 async def ws_handler(websocket, path):
     listener_task = asyncio.create_task(zmq_listener_task(websocket))
-    # ping_task = asyncio.create_task(ping(websocket))
+    ping_task = asyncio.create_task(ping(websocket))
     
     done, pending = await asyncio.wait(
-        [listener_task], # add ping_task here for testing
+        [listener_task, ping_task],
         return_when=asyncio.FIRST_COMPLETED,
     )
     
