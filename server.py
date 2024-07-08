cf957aad5ca18fa958a98cf490e2094104b30278
Joe Looney <hello@joelooney.org>
2024-07-07 22:30:04 -0400

    init

---
+++ server.py
@@ -0,0 +1,28 @@
+import asyncio
+import websockets
+import zmq
+import zmq.asyncio
+
+BITCOIN_ZMQ_ADDRESS = "tcp://0.0.0.0:9333"  # Change this to your Bitcoin ZMQ address
+WEBSOCKET_PORT = 8765
+
+async def zmq_listener():
+    context = zmq.asyncio.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect(BITCOIN_ZMQ_ADDRESS)
+    socket.setsockopt_string(zmq.SUBSCRIBE, '')
+
+    while True:
+        msg = await socket.recv_multipart()
+        yield msg
+
+async def ws_handler(websocket, path):
+    async for msg in zmq_listener():
+        await websocket.send(str(msg))
+
+async def main():
+    async with websockets.serve(ws_handler, "0.0.0.0", WEBSOCKET_PORT):
+        await asyncio.Future()  # Run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
